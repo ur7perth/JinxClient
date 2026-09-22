18348dc7b56d6a40461e8c5db2f2874a3d0c7b67
ur7perth <ur7.perth@gmail.com>
2026-09-22 05:57:41 +0300

    Create OldSwing.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/items/OldSwing.java
@@ -0,0 +1,12 @@
+package com.abo9kr.jinxclient.module.impl.items;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class OldSwing extends Module {
+
+    public OldSwing() {
+        super("OldSwing", Category.ITEMS,
+                "Uses the old (pre-1.9) hand swing animation for left-click / attacking.");
+    }
+}
