2ac947747f8b0de4a90622383a7a8361d56c68f4
ur7perth <ur7.perth@gmail.com>
2026-09-22 05:55:55 +0300

    Create TabPing.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/hud/TabPing.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.hud;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class TabPing extends Module {
+
+    public TabPing() {
+        super("TabPing", Category.HUD, "Shows player ping (in gray) on the Tab player list.");
+    }
+}
