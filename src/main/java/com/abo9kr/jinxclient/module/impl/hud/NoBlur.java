d1ffd90eb5b4b335df9ce123d1e6b3c5ba85b1c8
ur7perth <ur7.perth@gmail.com>
2026-09-22 05:55:01 +0300

    Create NoBlur.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/hud/NoBlur.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.hud;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class NoBlur extends Module {
+
+    public NoBlur() {
+        super("NoBlur", Category.HUD, "Removes the underwater/lava/pumpkin vision distortion shaders.");
+    }
+}
