71d79cdee4cefdc99f477f9471410716d5783a2b
ur7perth <ur7.perth@gmail.com>
2026-09-22 06:05:38 +0300

    Create NickHider.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/player/NickHider.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.player;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class NickHider extends Module {
+
+    public NickHider() {
+        super("NickHider", Category.PLAYER, "Hides/replaces your own name in client-side UI (e.g. for recording).");
+    }
+}
