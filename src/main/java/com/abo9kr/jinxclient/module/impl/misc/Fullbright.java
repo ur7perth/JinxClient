8cd65496f34be5ad18eb0749495493d0b1d1ce38
ur7perth <ur7.perth@gmail.com>
2026-09-22 05:59:48 +0300

    Create Fullbright.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/misc/Fullbright.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.misc;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class Fullbright extends Module {
+
+    public Fullbright() {
+        super("Fullbright", Category.MISC, "Maxes out your in-game brightness/gamma.");
+    }
+}
