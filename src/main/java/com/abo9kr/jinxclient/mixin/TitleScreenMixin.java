a6627f9b5508f21112734d34b70278c64f118a86
ur7perth <ur7.perth@gmail.com>
2026-09-22 05:49:19 +0300

    Create TitleScreenMixin.java

---
+++ src/main/java/com/abo9kr/jinxclient/mixin/TitleScreenMixin.java
@@ -0,0 +1,8 @@
+package com.abo9kr.jinxclient.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+}
