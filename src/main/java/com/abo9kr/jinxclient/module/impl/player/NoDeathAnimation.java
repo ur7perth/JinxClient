b0899346baf749e0adf1a51ba27b0b904234935d
ur7perth <ur7.perth@gmail.com>
2026-09-22 06:06:05 +0300

    Create NoDeathAnimation.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/player/NoDeathAnimation.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.player;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class NoDeathAnimation extends Module {
+
+    public NoDeathAnimation() {
+        super("NoDeathAnimation", Category.PLAYER, "Entities die instantly without playing the fall-over death animation.");
+    }
+}
