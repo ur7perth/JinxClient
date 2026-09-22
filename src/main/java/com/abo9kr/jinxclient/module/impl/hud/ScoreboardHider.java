7b0f903af6980da61dda3927180858ba1c961835
ur7perth <ur7.perth@gmail.com>
2026-09-22 05:55:26 +0300

    Create ScoreboardHider.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/hud/ScoreboardHider.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.hud;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class ScoreboardHider extends Module {
+
+    public ScoreboardHider() {
+        super("Scoreboard", Category.HUD, "Hides the sidebar scoreboard.");
+    }
+}
