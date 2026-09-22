d5ec8b80c8d21cf5a5b6ba0c1ed75c22302c28d0
ur7perth <ur7.perth@gmail.com>
2026-09-22 06:01:17 +0300

    Create NoPackWarn.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/misc/NoPackWarn.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.misc;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class NoPackWarn extends Module {
+
+    public NoPackWarn() {
+        super("NoPackWarn", Category.MISC, "Suppresses the server resource pack confirmation prompt.");
+    }
+}
