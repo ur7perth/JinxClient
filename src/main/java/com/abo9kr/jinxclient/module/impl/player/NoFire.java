d1aeca33bce938f14b298f89b0ad068f2e5ddeb6
ur7perth <ur7.perth@gmail.com>
2026-09-22 06:06:50 +0300

    Create NoFire.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/player/NoFire.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.player;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class NoFire extends Module {
+
+    public NoFire() {
+        super("NoFire", Category.PLAYER, "Hides the fire overlay covering your screen while burning.");
+    }
+}
