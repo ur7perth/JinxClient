079307313813843c08ce2e1115d0fe0454ae221c
ur7perth <ur7.perth@gmail.com>
2026-09-22 06:17:52 +0300

    Update KillEffect.java

--- src/main/java/com/abo9kr/jinxclient/module/impl/misc/KillEffect.java
+++ src/main/java/com/abo9kr/jinxclient/module/impl/misc/KillEffect.java
@@ -11,10 +11,10 @@ public class KillEffect extends Module {
 
     public final ModeSetting effect = register(new ModeSetting(
             "Effect", List.of("None", "Totem", "Anvil", "TNT", "Lightning"), "None"));
-    public final NumberSetting tntSpeed = register(new NumberSetting("TNT Speed", 1.0, 0.25, 3.0, 0.25))
-            .visibleIf(() -> effect.getValue().equals("TNT"));
+    public final NumberSetting tntSpeed = register(new NumberSetting("TNT Speed", 1.0, 0.25, 3.0, 0.25));
 
     public KillEffect() {
         super("KillEffect", Category.MISC, "Plays a cosmetic particle effect where a player you killed died.");
+        tntSpeed.visibleIf(() -> effect.getValue().equals("TNT"));
     }
 }
