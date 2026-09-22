c36dfa64b8a650708f995c7ed1fe3c0660335539
ur7perth <ur7.perth@gmail.com>
2026-09-22 05:57:15 +0300

    Create NoEatAnimation.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/items/NoEatAnimation.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.items;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class NoEatAnimation extends Module {
+
+    public NoEatAnimation() {
+        super("NoEatAnimation", Category.ITEMS, "Removes the hand-bobbing animation while eating/drinking.");
+    }
+}
