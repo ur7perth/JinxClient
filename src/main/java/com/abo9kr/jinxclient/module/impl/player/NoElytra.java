fc2784233cd72840be5507908aa7825d04e64198
ur7perth <ur7.perth@gmail.com>
2026-09-22 06:06:25 +0300

    Create NoElytra.java

---
+++ src/main/java/com/abo9kr/jinxclient/module/impl/player/NoElytra.java
@@ -0,0 +1,11 @@
+package com.abo9kr.jinxclient.module.impl.player;
+
+import com.abo9kr.jinxclient.module.Category;
+import com.abo9kr.jinxclient.module.Module;
+
+public class NoElytra extends Module {
+
+    public NoElytra() {
+        super("NoElytra", Category.PLAYER, "Hides the elytra model rendered on other players.");
+    }
+}
